ce17d46943ad7f97d08f89d536ccc9b5b9640166
JugglypuffM <stas.klinin@gmail.com>
2024-02-20 22:41:07 +0500

    Клинин Станислав exercises01

--- exercises01/src/main/scala/exercises01/Vector.scala
+++ exercises01/src/main/scala/exercises01/Vector.scala
@@ -7,35 +7,30 @@ class Vector(val x: Double, val y: Double) {
 
   def *(scalar: Double): Vector = new Vector(scalar*x, scalar*y)
 
-  def unary_- : Vector = new Vector(-x, -y)
+  def unary_- : Vector = this*(-1)
 
   def euclideanLength: Double = math.sqrt(x*x + y*y)
 
-  def normalized: Vector = new Vector(x/euclideanLength, y/euclideanLength)
+  def normalized: Vector = {
+    val el = euclideanLength
+    new Vector(x / el, y / el)
+  }
 
   override def equals(other: Any): Boolean = {
     other match {
-      case other_vector: Vector =>
-        if (x.equals(other_vector.x) && y.equals(other_vector.y)) {
-          return true
-        }
-      case _ =>
+      case otherVector: Vector => x.equals(otherVector.x) && y.equals(otherVector.y)
+      case _ => false
     }
-    false
   }
 
   // Vector(x, y)
-  override def toString: String = "Vector(" + x + ", " + y + ")"
+  override def toString: String = s"Vector($x, $y)"
 }
 
 object Vector {
   def fromAngle(angle: Double, length: Double): Vector = new Vector(math.cos(angle)*length, math.sin(angle)*length)
 
-  def sum(list: List[Vector]): Vector = {
-    var vector = new Vector(0, 0)
-    for (other <- list) vector += other
-    vector
-  }
+  def sum(list: List[Vector]): Vector = list.foldLeft(new Vector(0, 0))((x, y) => x + y)
 
   def unapply(arg: Vector): Option[(Double, Double)] = Some(arg.x, arg.y)
 }
