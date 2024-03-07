541ad0363351d318567bfb3cc235cdb10b5509a8
JugglypuffM <stas.klinin@gmail.com>
2024-03-07 10:24:49 +0500

    Клинин Станислав exercises03

--- exercises03/src/main/scala/exercises03/MyList.scala
+++ exercises03/src/main/scala/exercises03/MyList.scala
@@ -3,6 +3,7 @@ package exercises03
 import exercises03.MyList.Filter.{Preserve, Skip}
 
 import scala.annotation.tailrec
+import scala.runtime.Nothing$
 
 sealed trait MyList[+A]
 
@@ -15,57 +16,26 @@ object MyList {
   def foldLeft[A, B](list: MyList[A])(base: B)(f: (B, A) => B): B =
     list match {
       case Cons(head, tail) => foldLeft(tail)(f(base, head))(f)
-      case Nil              => base
+      case Nil => base
     }
 
-  def sum(list: MyList[Int]): Int = foldLeft(list)(0)(_ + _)
+  def sum(list: MyList[Int]): Int = foldLeft(list)(0)(_+_)
 
-  def reverse[A](list: MyList[A]): MyList[A] = {
-    @tailrec
-    def inner(list: MyList[A], acc: MyList[A]): MyList[A] = list match {
-      case Cons(head, tail) => inner(tail, Cons(head, acc))
-      case Nil              => acc
-    }
-    inner(list, Nil)
-  }
+  def reverse[A](list: MyList[A]): MyList[A] = foldLeft(list)(Nil: MyList[A])((B, A) => Cons(A, B))
 
-  def last[A](myList: MyList[A]): Option[A] = {
-    @tailrec
-    def inner(list: MyList[A], acc: Option[A]): Option[A] = list match {
-      case Cons(head, tail) => val tmp = Option(head); inner(tail, tmp)
-      case Nil              => acc
-    }
-    inner(myList, None)
-  }
+  def last[A](myList: MyList[A]): Option[A] = foldLeft(myList)(None: Option[A])((B, A) => Some(A))
 
-  def size[A](myList: MyList[A]): Int = foldLeft(myList)(0)((B, _) => B + 1)
+  def size[A](myList: MyList[A]): Int = foldLeft(myList)(0)((B, A) => B+1)
 
-  def max[A](myList: MyList[A], isBigger: (A, A) => Boolean): Option[A] = {
-    @tailrec
-    def inner(list: MyList[A], acc: Option[A]): Option[A] = list match {
-      case Cons(head, tail) =>
-        val tmp = acc match {
-          case Some(v) => if (isBigger(v, head)) Some(v) else Some(head)
-          case None    => Some(head)
-        }
-        inner(tail, tmp)
-      case Nil => acc
-    }
-    inner(myList, None)
-  }
+  def max[A](myList: MyList[A], isBigger: (A, A) => Boolean): Option[A] = foldLeft(myList)(None: Option[A])((A1, A2) => A1 match {
+    case Some(v) => if (isBigger(v, A2)) Some(v) else Some(A2)
+    case None => Some(A2)
+  })
 
-  def filter[A](myList: MyList[A], predicate: A => Filter.Filter): MyList[A] = {
-    @tailrec
-    def inner(list: MyList[A], acc: MyList[A]): MyList[A] = list match {
-      case Cons(head, tail) =>
-        predicate(head) match {
-          case Preserve => inner(tail, Cons(head, acc))
-          case Skip     => inner(tail, acc)
-        }
-      case Nil => acc
-    }
-    reverse(inner(myList, Nil))
-  }
+  def filter[A](myList: MyList[A], predicate: A => Filter.Filter): MyList[A] = reverse(foldLeft(myList)(Nil: MyList[A])((B, A) => predicate(A) match {
+    case Preserve => Cons(A, B)
+    case Skip => B
+  }))
 
   object Filter {
     sealed trait Filter
--- exercises03/src/main/scala/exercises03/Partial.scala
+++ exercises03/src/main/scala/exercises03/Partial.scala
@@ -3,5 +3,4 @@ package exercises03
 object Partial {
   def combo[I, T](funcs: List[PartialFunction[I, T]]): I => Option[T] =
     func => funcs.map(_.lift(func)).collectFirst({ case Some(res) => res })
-
 }
