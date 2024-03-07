c8b1e6d4839ca2b0b3e9bee39a41968112e502ef
JugglypuffM <stas.klinin@gmail.com>
2024-03-07 10:27:41 +0500

    Клинин Станислав exercises03

--- exercises03/src/main/scala/exercises03/MyList.scala
+++ exercises03/src/main/scala/exercises03/MyList.scala
@@ -16,26 +16,34 @@ object MyList {
   def foldLeft[A, B](list: MyList[A])(base: B)(f: (B, A) => B): B =
     list match {
       case Cons(head, tail) => foldLeft(tail)(f(base, head))(f)
-      case Nil => base
+      case Nil              => base
     }
 
-  def sum(list: MyList[Int]): Int = foldLeft(list)(0)(_+_)
+  def sum(list: MyList[Int]): Int = foldLeft(list)(0)(_ + _)
 
   def reverse[A](list: MyList[A]): MyList[A] = foldLeft(list)(Nil: MyList[A])((B, A) => Cons(A, B))
 
   def last[A](myList: MyList[A]): Option[A] = foldLeft(myList)(None: Option[A])((B, A) => Some(A))
 
-  def size[A](myList: MyList[A]): Int = foldLeft(myList)(0)((B, A) => B+1)
+  def size[A](myList: MyList[A]): Int = foldLeft(myList)(0)((B, A) => B + 1)
 
-  def max[A](myList: MyList[A], isBigger: (A, A) => Boolean): Option[A] = foldLeft(myList)(None: Option[A])((A1, A2) => A1 match {
-    case Some(v) => if (isBigger(v, A2)) Some(v) else Some(A2)
-    case None => Some(A2)
-  })
+  def max[A](myList: MyList[A], isBigger: (A, A) => Boolean): Option[A] =
+    foldLeft(myList)(None: Option[A])((A1, A2) =>
+      A1 match {
+        case Some(v) => if (isBigger(v, A2)) Some(v) else Some(A2)
+        case None    => Some(A2)
+      }
+    )
 
-  def filter[A](myList: MyList[A], predicate: A => Filter.Filter): MyList[A] = reverse(foldLeft(myList)(Nil: MyList[A])((B, A) => predicate(A) match {
-    case Preserve => Cons(A, B)
-    case Skip => B
-  }))
+  def filter[A](myList: MyList[A], predicate: A => Filter.Filter): MyList[A] =
+    reverse(
+      foldLeft(myList)(Nil: MyList[A])((B, A) =>
+        predicate(A) match {
+          case Preserve => Cons(A, B)
+          case Skip     => B
+        }
+      )
+    )
 
   object Filter {
     sealed trait Filter
