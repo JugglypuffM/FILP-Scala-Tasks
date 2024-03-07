665e4a01c074a73b2280a28983ddba73717803e8
JugglypuffM <stas.klinin@gmail.com>
2024-03-07 09:16:39 +0500

    Клинин Станислав exercises03

--- exercises03/src/main/scala/exercises03/Partial.scala
+++ exercises03/src/main/scala/exercises03/Partial.scala
@@ -2,6 +2,6 @@ package exercises03
 
 object Partial {
   def combo[I, T](funcs: List[PartialFunction[I, T]]): I => Option[T] =
-    I => funcs.map(_.lift(I)).collectFirst({ case Some(res) => res })
+    func => funcs.map(_.lift(func)).collectFirst({ case Some(res) => res })
 
 }
--- exercises03/src/main/scala/exercises03/SetFunctions.scala
+++ exercises03/src/main/scala/exercises03/SetFunctions.scala
@@ -7,17 +7,17 @@ object SetFunctions {
 
   def contains[A](s: Set[A], elem: A): Boolean = s(elem)
 
-  def singletonSet[A](elem: A): Set[A] = A => A == elem
+  def singletonSet[A](elem: A): Set[A] = a => a == elem
 
-  def union[A](s: Set[A], t: Set[A]): Set[A] = A => s(A) || t(A)
+  def union[A](s: Set[A], t: Set[A]): Set[A] = a => s(a) || t(a)
 
-  def intersect[A](s: Set[A], t: Set[A]): Set[A] = A => s(A) && t(A)
+  def intersect[A](s: Set[A], t: Set[A]): Set[A] = a => s(a) && t(a)
 
-  def diff[A](s: Set[A], t: Set[A]): Set[A] = A => s(A) && !intersect(s, t)(A)
+  def diff[A](s: Set[A], t: Set[A]): Set[A] = a => s(a) && !intersect(s, t)(a)
 
-  def symmetricDiff[A](s: Set[A], t: Set[A]): Set[A] = A => union(s, t)(A) && !intersect(s, t)(A)
+  def symmetricDiff[A](s: Set[A], t: Set[A]): Set[A] = a => union(s, t)(a) && !intersect(s, t)(a)
 
-  def filter[A](s: Set[A], p: A => Boolean): Set[A] = A => p(A)
+  def filter[A](s: Set[A], p: A => Boolean): Set[A] = a => p(a)
 
-  def cartesianProduct[A, B](as: Set[A], bs: Set[B]): Set[(A, B)] = A => as(A._1) && bs(A._2)
+  def cartesianProduct[A, B](as: Set[A], bs: Set[B]): Set[(A, B)] = a => as(a._1) && bs(a._2)
 }
