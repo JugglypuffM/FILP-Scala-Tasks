2d8a217ed716c5259c444bc00eb1555fed4789d6
JugglypuffM <stas.klinin@gmail.com>
2024-03-28 11:07:54 +0500

    Клинин Станислав exercises05

--- exercises05/src/main/scala/exercises06/e1_list_ops/ListOps.scala
+++ exercises05/src/main/scala/exercises06/e1_list_ops/ListOps.scala
@@ -2,14 +2,16 @@ package exercises06.e1_list_ops
 
 import scala.Integral.Implicits.infixIntegralOps
 
+class ListOps[A: Integral](list: List[A]) {
+  private val divider: A  = Integral[A].fromInt(2)
+  private val zero: A     = Integral[A].fromInt(0)
+  def filterOdd: List[A]  = list.filter(_ % divider != zero)
+  def filterEven: List[A] = list.filter(_ % divider == zero)
+}
+
 object Examples {
   // сделайте так, чтобы скомпилировалось
-  implicit class listOps[A: Integral](list: List[A]) {
-    private val divider: A  = Integral[A].fromInt(2)
-    private val zero: A     = Integral[A].fromInt(0)
-    def filterOdd: List[A]  = list.filter(_ % divider != zero)
-    def filterEven: List[A] = list.filter(_ % divider == zero)
-  }
+  implicit def listOps[A: Integral](list: List[A]): ListOps[A] = new ListOps[A](list)
 
   List[Int](1, 2, 3).filterOdd
   List[Int](1, 2, 3).filterEven
